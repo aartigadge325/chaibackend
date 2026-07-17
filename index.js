438b8027dd64192e58f6360dcb81aa991c324554
aartigadge325 <aartigadge325@gmail.com>
2026-07-17 21:46:59 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const githubData = 
+{
+    name: "Aarti Gadge",
+    github: "https://github.com/aartigadge325"
+};
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -18,6 +24,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send('<h2>ChaiAurCode</h2>');
+});
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 });
 
 app.listen(process.env.PORT, () => {
